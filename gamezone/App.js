9483e1b40b1ce9dd309d7c0296ae31edade1f847
RukshanNuwan <39560159+RukshanNuwan@users.noreply.github.com>
2020-12-12 12:28:30 +0530

    custom fonts

--- gamezone/App.js
+++ gamezone/App.js
@@ -1,12 +1,35 @@
 // @author phoenix|201212_0843
 
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 // custom components
 import Home from './screens/home';
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// function area
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+});
+
+
 export default function App() {
-  return (
-    <Home />
-  );
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (fontLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.error}
+      />
+    );
+  }
 }
