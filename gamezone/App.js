297fab7c066ef277405a3b97679f6f33f6d9c413
RukshanNuwan <39560159+RukshanNuwan@users.noreply.github.com>
2020-12-12 09:51:14 +0530

    starting review app

--- gamezone/App.js
+++ gamezone/App.js
@@ -1,23 +1,12 @@
 // @author phoenix|201212_0843
 
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+// custom components
+import Home from './screens/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
