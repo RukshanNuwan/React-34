9454c3cb52b04ba8bc21c3492e89a52e84545331
RukshanNuwan <39560159+RukshanNuwan@users.noreply.github.com>
2020-12-13 13:12:37 +0530

    using images

--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 // custom
-import { globalStyles } from '../styles/global';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetails({ navigation }) {
     // function area
-    const pressHandler = () => {
-        navigation.goBack();
-    }
+    const rating = navigation.getParam('rating');
 
 
     return (
@@ -17,8 +15,22 @@ export default function ReviewDetails({ navigation }) {
             <Card>
                 <Text style={globalStyles.text}>{navigation.getParam('title')}</Text>
                 <Text style={globalStyles.text}>{navigation.getParam('body')}</Text>
-                <Text style={globalStyles.text}>{navigation.getParam('rating')}</Text>
+                <View style={styles.rating}>
+                    <Text>GameZone rating: </Text>
+                    <Image source={images.ratings[rating]} />
+                </View>
             </Card>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    rating: {
+        marginTop: 16,
+        paddingTop: 16,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        borderTopWidth: 1,
+        borderTopColor: '#eee'
+    }
+});
