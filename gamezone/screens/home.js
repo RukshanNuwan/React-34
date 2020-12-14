a02de600a6795e00495f010ea6531e6d91c0aca8
RukshanNuwan <39560159+RukshanNuwan@users.noreply.github.com>
2020-12-14 14:21:51 +0530

    modals

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Modal } from 'react-native';
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import { MaterialIcons } from '@expo/vector-icons';
 
 // custom
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
 
 export default function Home({ navigation }) {
+    const [modalOpen, setModalOpen] = useState(false);
+
     const [reviews, setReviews] = useState([
         { title: 'Zelda, Breath of fresh air', rating: 5, body: 'lorem ipsum', key: '1' },
         { title: 'Gotta catch the all (again)', rating: 4, body: 'lorem ipsum', key: '2' },
@@ -16,6 +19,26 @@ export default function Home({ navigation }) {
 
     return (
         <View style={globalStyles.container}>
+            <Modal visible={modalOpen} animationType='fade'>
+                <View style={styles.modalContent}>
+                    <MaterialIcons
+                        name='close'
+                        size={24}
+                        onPress={() => setModalOpen(false)}
+                        // multiple styles
+                        style={{ ...styles.modalToggle, ...styles.modalClose }}
+                    />
+                    <Text>hello from the modal</Text>
+                </View>
+            </Modal>
+
+            <MaterialIcons
+                name='add'
+                size={24}
+                onPress={() => setModalOpen(true)}
+                style={styles.modalToggle}
+            />
+
             <FlatList
                 data={reviews}
                 renderItem={({ item }) => (
@@ -29,3 +52,23 @@ export default function Home({ navigation }) {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    modalToggle: {
+        marginBottom: 10,
+        padding: 10,
+        alignSelf: 'center',
+        borderWidth: 1,
+        borderColor: '#aaa',
+        borderRadius: 5
+    },
+
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0
+    },
+
+    modalContent: {
+        flex: 1
+    }
+});
