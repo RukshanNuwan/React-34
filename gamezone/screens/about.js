3062e9d9750fee662da7ffea0c275d539db1e2dd
RukshanNuwan <39560159+RukshanNuwan@users.noreply.github.com>
2020-12-16 17:29:03 +0530

    Finish

--- gamezone/screens/about.js
+++ gamezone/screens/about.js
@@ -7,7 +7,15 @@ import { globalStyles } from '../styles/global';
 export default function About() {
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.text}>About Screen</Text>
+            <Text style={globalStyles.text}>This is an app which give me some great basic knowledge to learn react native app development. Thank you!</Text>
+            <Text style={styles.signature}>&copy; Phoenix|2020</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    signature: {
+        marginTop: 10,
+        textAlign: 'center'
+    }
+});
